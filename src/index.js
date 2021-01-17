fe9e9c4ad69bda90b2e2314cdbbfb0ad75354362
MykhailKo <tuareg1812@gmail.com>
2021-01-17 16:52:26 +0200

    start script created

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 const app = require('./app')
-const port = process.env.PORT
-
+const port = process.env.npm_config_port || process.env.PORT || 3000
+// start command with port: npm --port=${your port} run start
 app.listen(port, () => {
   console.log(`App is running on port ${port}`)
 })
